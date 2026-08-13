eb7c8a5d8390a59a3a98b00cbbf648e55da9b1fa
bankotsu-dev <bankotsugamer@gmail.com>
2026-08-12 22:56:46 -0700

    fix: correct spelling in AppLogo and NavMain components

--- resources/js/components/app-logo.tsx
+++ resources/js/components/app-logo.tsx
@@ -7,7 +7,7 @@ export default function AppLogo() {
                 <AppLogoIcon className="size-7 fill-current text-white dark:text-black" />
             </div>
             <div className="ml-1 grid flex-1 text-left text-sm">
-                <span className="mb-0.5 truncate leading-none font-semibold">My Backlist</span>
+                <span className="mb-0.5 truncate leading-none font-semibold">My Backlog</span>
             </div>
         </>
     );
